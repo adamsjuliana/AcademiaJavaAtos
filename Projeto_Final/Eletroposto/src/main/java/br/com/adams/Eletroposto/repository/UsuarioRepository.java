28628e016c43887ca32fdb72787884ef295ec422
Juliana Adams <julianapadams@gmail.com>
2022-07-15 01:47:55 -0300

    -

--- Projeto_Final/Eletroposto/src/main/java/br/com/adams/Eletroposto/repository/UsuarioRepository.java
+++ Projeto_Final/Eletroposto/src/main/java/br/com/adams/Eletroposto/repository/UsuarioRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.adams.Eletroposto.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-
+	Usuario findByUsername(String username);
 }
